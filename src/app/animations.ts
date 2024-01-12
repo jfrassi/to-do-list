29b7acbb6882a891cc2bda06d61f9c758c62b65b
Joao Gouvea <joaofrassi.dev@gmail.com>
2024-01-12 13:01:53 -0300

    curso de animações utilizando angular da alura

---
+++ src/app/animations.ts
@@ -0,0 +1,40 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
+
+
+export const highlightedStateTrigger = trigger('highlightedState', [
+  state('default', style({
+    border: '1.5px solid #b2b6ff',
+
+  })),
+  state('highlighted', style({
+    border: '2.3px solid #b2b6ff',
+    filter: 'brightness(97%)'
+
+  })),
+  transition('default => highlighted', [
+    animate('400ms ease-in-out', style({
+      transform: 'scale(1.02)'
+    })),
+
+    animate('500ms')
+  ])
+])
+
+
+export const shownStateTrigger = trigger('shownState', [
+
+  transition(':enter', [
+  style({
+  opacity: 0
+  }),
+  animate(300, style({
+  opacity: 1
+}))
+]),
+transition(':leave', [
+animate(300, style({
+  opacity: 0
+}))
+])
+])
+
